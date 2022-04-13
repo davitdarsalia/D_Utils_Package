16dab177b50401524206b1d4a0104b3782f35b3e
davitdarsalia <darsalia.david1998@gmail.com>
2022-04-13 12:40:29 +0400

    feat: Tests For Different Types <RemoveDuplicates>

--- sliceUtils/sliceUtils_test.go
+++ sliceUtils/sliceUtils_test.go
@@ -86,3 +86,64 @@ func TestSortArrayDescending(t *testing.T) {
 		}
 	}
 }
+
+// Test For Different Types
+func TestRemoveDuplicatesInts(t *testing.T) {
+	type testTemplate struct {
+		input []int
+		want  []int
+	}
+	testCases := []testTemplate{
+		{input: []int{1, 1, 3, 3, 5, 5, 31, 61, 123, 87}, want: []int{1, 3, 5, 31, 61, 123, 87}},
+		{input: []int{}, want: []int{}},
+		{input: []int{1, 2}, want: []int{1, 2}},
+		{input: []int{5}, want: []int{5}},
+	}
+
+	for _, tc := range testCases {
+		result := RemoveDuplicates(tc.want)
+		if !reflect.DeepEqual(tc.want, result) {
+			t.Fatalf("Expected: %v, But Got: %v", tc.want, result)
+		}
+	}
+}
+
+func TestRemoveDuplicatesStrings(t *testing.T) {
+	type testTemplate struct {
+		input []string
+		want  []string
+	}
+	testCases := []testTemplate{
+		{input: []string{"Test1", "Test1", "Test2", "Test3"}, want: []string{"Test1", "Test2", "Test3"}},
+		{input: []string{"A", "B", "A", "D", "D"}, want: []string{"A", "B", "D"}},
+		{input: []string{}, want: []string{}},
+		{input: []string{"Standalone Test"}, want: []string{"Standalone Test"}},
+	}
+
+	for _, tc := range testCases {
+		result := RemoveDuplicates(tc.want)
+		if !reflect.DeepEqual(tc.want, result) {
+			t.Fatalf("Expected: %v, But Got: %v", tc.want, result)
+		}
+	}
+}
+
+func TestRemoveDuplicatesBooleans(t *testing.T) {
+	type testTemplate struct {
+		input []bool
+		want  []bool
+	}
+	testCases := []testTemplate{
+		{input: []bool{true, true, false, false, true}, want: []bool{true, false}},
+		{input: []bool{}, want: []bool{}},
+		{input: []bool{true}, want: []bool{true}},
+		{input: []bool{false}, want: []bool{false}},
+	}
+
+	for _, tc := range testCases {
+		result := RemoveDuplicates(tc.want)
+		if !reflect.DeepEqual(tc.want, result) {
+			t.Fatalf("Expected: %v, But Got: %v", tc.want, result)
+		}
+	}
+}
